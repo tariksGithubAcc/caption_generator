8fa3628dcc19edf1099dbc5f6f549947d7f3ee4e
Tarik <104720358+tariksGithubAcc@users.noreply.github.com>
2023-07-07 15:43:41 +0530

    Add files via upload

--- wsgi.py
+++ wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for whoareyou project.
+WSGI config for business_lead project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'whoareyou.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'business_lead.settings')
 
 application = get_wsgi_application()
